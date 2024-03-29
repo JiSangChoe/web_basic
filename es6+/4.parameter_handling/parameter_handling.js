fb2cfd16743e2cba8bbcf001ae8a065b73806b7a
JiSangChoe <chlwltkd123@gmail.com>
2024-03-29 11:50:30 +0900

    2024.03.29.11:50 매개변수 spread 연산자 디스트럭처링

--- es6+/4.parameter_handling/parameter_handling.js
+++ es6+/4.parameter_handling/parameter_handling.js
@@ -6,26 +6,90 @@ const add = (a = 10, b = 20) => { return a + b };
 console.log(add());
 console.log(add(30, 90));
 
-
 console.log('========================================');
 
 /*
     rest 매개변수 : 
-    - 
-    - 
-    - 
+    - 매개변수 앞에 ...을 붙여서 정의한 매개변수
+    - 함수에 전달받은 인수의 목록을 배열로 전달
+    - rest 매개변수는 반드시 제일 마지막에 선언되어야 함
 */
+const restFunction1 = (...args) => {
+    console.log(args);
+};
+restFunction1(1, 2, 3, 4, 5);
 
+// ...args 는 제일 마지막에 선언되어야 함
+const restFunction2 = (arg1, ...args) => {
+    console.log(arg1);  // 1
+    console.log(args); // [2, 3, 4, 5] -> 나머지 부분을 배열로 가져옴
+};
+restFunction2(1, 2, 3, 4, 5);
 
 
 console.log('========================================');
 
 /*
     spread 연산자 : 
-    - 
-    - 
+    - ...를 사용하여 대상을 개별 요소로 분리
+    - 반드시 피연산자를 반복 가능한 객체 (배열, 객체, 문자열 등...) 이어야 함
 */
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
 
-
+console.log(...numbers); // -> 배열의 형태가 아닌 각 요소 자체를 꺼내와서 출력
+console.log(1, 2, 3, 4, 5); // -> 바로 위의 코드와 같음
 
 console.log('========================================');
+
+// 배열 복사에 사용
+const copyNumbers = [ ...numbers ];
+console.log(copyNumbers);
+
+copyNumbers[0] = 10;
+console.log(numbers);
+console.log(copyNumbers); // -> 새로운 주소에 할당된 배열의 값이 바뀐걸 알 수 있음
+
+console.log('========================================');
+
+// 배열 연결에 사용
+const numbers2 = [6, 7, 8, 9, 10];
+let newNumbers = [ ...numbers, ...numbers2 ];
+console.log(newNumbers);
+newNumbers = [ ...numbers2, ...numbers ]; // -> 순서가 바뀜
+console.log(newNumbers);
+
+console.log('========================================');
+
+//! 배열 요소 추가에 사용
+newNumbers = [ ...numbers, 0, -1, -2];
+console.log(newNumbers);
+
+// 객체 복사, 연결, 요소 추가에 사용
+const king = {
+    name: '이성계',
+    tomName: '태조'
+};
+
+// 복사
+let newKing = { ...king };
+newKing.name = '이방과';
+console.log(newKing); // 새로운 주소에 복사했기 때문에 king의 이름은 바뀌지 않는다.
+console.log(king);
+
+const kingInfo = {
+    address: '서울특별시',
+    country: '고려'
+}
+
+// 연결
+newKing = { ...king, ...kingInfo };
+console.log(newKing);
+
+// 요소 추가
+newKing = { ...king, birth: '1335.11.04'};
+console.log(newKing);
+
+//! king에는 이미 name 있는데 뒤에 name이 오면 복사변경으로 인식해서 바뀌고 그다음 뒤에 요소를 추가, 즉 덮어써짐
+newKing = { ...king, name: '이단', birth: '1335.11.04' };
+console.log(newKing);
--- es6+/6.destructuring/destructuring.js
+++ es6+/6.destructuring/destructuring.js
@@ -1,16 +1,40 @@
 /*
-    디스트럭처링 :
-    - 
-    - 
+    디스트럭처링 : 비구조화 할당
+    - 구조화된 배열 또는 객체를 비구조화, 구조화 파괴하여 개별 변수로 할당하는 것
+    - 배열 또는 객체에서 필요한 데이터만 추출해서 변수에 할당하여 사용할 수 있도록 함
 */
+
+const king = {
+    name: '이성계',
+    tombName: '태조',
+    birth: '1335.11.04.',
+    death: '1408.06.27.'
+};
+
+// let name = king.name;
+// let tombName = king.tombName;
+// let birth = king.birth;
+// let death = king.death;
+
+let fruits = ['apple', 'banana', 'melon', 'mango', 'orange'];
+
+// let fruits1 = fruits[0];
+// let fruits2 = fruits[1];
+// let fruits3 = fruits[2];
+// let fruits4 = fruits[3];
+// let fruits5 = fruits[4];
 
 /*
     배열 디스트럭처링
-    - 
-    - 
+    - 배열의 각각의 요소를 배열로부터 추출해서 변수 리스트에 할당
+    - 할당 기준은 인덱스
 */
-
-
+// let fruits = ['apple', 'banana', 'melon', 'mango', 'orange'];
+const [fruits1, fruits2, fruits3] = fruits;
+console.log(fruits1);
+console.log(fruits2);
+console.log(fruits3);
+console.log(fruits);
 
 console.log('========================================');
 

7864537c226aba36d8a983e1fc2eaaad61b57e79
JiSangChoe <chlwltkd123@gmail.com>
2024-04-02 09:50:35 +0900

    2024.04.02.09:50 typescript 환경설정

--- ts/2.type/type.ts
+++ ts/2.type/type.ts
@@ -2,8 +2,18 @@
     타입선언 : 
     - 변수 및 함수에 타입을 지정하여 타입에 대한 제한을 추가할 수 있음
 */
+let variable: string = '문자열';
+// variable = true
 
+// 함수
+function func(arg:number): string {
+    return arg + '';
+}
 
+// 화살표 함수
+const arrowFunc = (arg: number): string => {
+    return arg + '';
+}
 
 /*
     typescript의 타입
